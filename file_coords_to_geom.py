de0d375e3eca0d1a625f7748e3ce8b733756321e
Pszczoła <dpszczola@multiconsult.no>
2021-12-08 14:49:24 +0100

    add exercise 4

--- file_coords_to_geom.py
+++ file_coords_to_geom.py
@@ -1,8 +1,9 @@
 import pandas as pd
-from shapely.geometry import Point
+from shapely.geometry import Point, LineString
 
 orig_points = []
 dest_points = []
+
 
 df = pd.read_csv('travelTimes_2015_Helsinki.txt', sep = ';')
 
@@ -20,3 +21,23 @@ for (x,y) in zip(from_x,from_y):
 #iterate over dest
 for (x,y) in zip(to_x,to_y):
     dest_points.append(Point(x, y))
+
+#Problem 4 SOLUTION
+
+
+lines = []
+lines2 = []
+
+for (p1, p2) in zip(orig_points, dest_points):
+    lines.append(LineString([p1, p2]))
+
+
+def avgDist(arg1, arg2):
+    b = 0
+    for (arg1, arg2) in zip(arg1, arg2):
+        lines2.append(LineString([arg1, arg2]))
+
+    for line in lines2:
+        b = b + line.length
+    return b / len(lines2)
+
